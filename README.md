3ded86a64ccba18e0fe1da1004a1f16378804293
nwn8 <s582031@nwmissouri.edu>
2025-07-18 08:47:46 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cintel-o5-cintel
+
+This project will simulate Streaming data using the Python deque 
---
+++ dashboard/app.py
@@ -0,0 +1,71 @@
+from shiny import reactive, render
+from shiny.express import ui
+import random
+from datetime import datetime
+from faicons import icon_svg
+
+
+UPDATE_INTERVAL_SECS: int = 1
+
+@reactive.calc()
+def reactive_calc_combined():
+
+    # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
+
+    # Data generation logic. Get random between -18 and -16 C, rounded to 1 decimal place
+    temp = round(random.uniform(-18, -16), 1)
+
+    # Get a timestamp for "now" and use string format strftime() method to format it
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    latest_dictionary_entry = {"temp": temp, "timestamp": timestamp}
+
+    # Return everything we need
+    return latest_dictionary_entry
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Page Options
+# ------------------------------------------------
+
+
+ui.page_opts(title="PyShiny Express: Live Data (Basic)", fillable=True)
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Sidebar
+# ------------------------------------------------
+
+with ui.sidebar(open="open"):
+    ui.h2("Antarctic Explorer", class_="text-center")
+    ui.p(
+        "A demonstration of real-time temperature readings in Antarctica.",
+        class_="text-center",
+    )
+
+
+#---------------------------------------------------------------------
+# In Shiny Express, everything not in the sidebar is in the main panel
+#---------------------------------------------------------------------
+
+
+ui.h2("Current Temperature")
+
+@render.text
+def display_temp():
+    """Get the latest reading and return a temperature string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['temp']} C"
+
+ui.p("warmer than usual")
+
+icon_svg("sun")
+
+ui.hr()
+
+ui.h2("Current Date and Time")
+
+@render.text
+def display_time():
+    """Get the latest reading and return a timestamp string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['timestamp']}"
